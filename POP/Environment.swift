97db94807820eb58a9a43883b45bdd18e3be8428
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-09-02 02:02:41 -0300

    Finalizando desafio

--- POP/Environment.swift
+++ POP/Environment.swift
@@ -17,10 +17,17 @@ protocol Photosyntesis {
 }
 
 enum Habitat {
-    case forest
-    case savanna
-    case montain
-    case desert
+    case forest,
+         savanna,
+         montain,
+         desert
+}
+
+enum Dietary {
+    case carnivore,
+         herbivore,
+         omnivere
+
 }
 
 protocol Organism {
@@ -31,28 +38,44 @@ protocol Organism {
 class Animal: Organism, Movable{
     let name: String
     let habitat: Habitat
+    let dietary: Dietary
+    var commonFood: [String]
     
-    init(name: String, habitat: Habitat) {
+    init(name: String, habitat: Habitat, dietary: Dietary, commonFood: [String]) {
         self.name = name
         self.habitat = habitat
+        self.dietary = dietary
+        self.commonFood = commonFood
     }
     
     func move() {
         print("\(name) is moving")
     }
     
-    func eat() {
-        print("\(name) is eating")
+    func running() {
+        print("\(name) is running")
+    }
+    
+    func eat(_ organism: String) {
+        print("\(name) is eating \(organism)")
+    }
+    
+    func drinkWater() {
+        print("\(name) is drinking water")
     }
 }
 
 class Plant: Organism, Photosyntesis {
     let name: String
     let habitat: Habitat
+    let fruit: Bool
+    let berry: String?
     
-    init(name: String, habitat: Habitat) {
+    init(name: String, habitat: Habitat, fruit: Bool, berry: String? = nil) {
         self.name = name
         self.habitat = habitat
+        self.fruit = fruit
+        self.berry = fruit ?  berry : nil
     }
     
     func performPhotosyntesis() {
@@ -60,18 +83,48 @@ class Plant: Organism, Photosyntesis {
     }
 }
 
-let lion = Animal(name: "Lion", habitat: .savanna)
-let wolf = Animal(name: "Wolf", habitat: .montain)
-let tree = Plant(name: "Tree", habitat: .forest)
-let cactus = Plant(name: "Cactus", habitat: .desert)
+let lion = Animal(name: "Lion", habitat: .savanna, dietary: .carnivore, commonFood: ["Giraffe", "Deer", "Zebra"])
+let wolf = Animal(name: "Wolf", habitat: .montain, dietary: .carnivore, commonFood: ["Rabits", "Montain Deer", "Squirrel"])
+let squirrel = Animal(name: "Squirrel", habitat: .montain, dietary: .herbivore, commonFood: ["Pine", "Juglans"])
+let deer = Animal(name: "Montain Deer", habitat: .montain, dietary: .herbivore, commonFood: ["Browse"])
+
+let tree = Plant(name: "Orange Tree", habitat: .forest, fruit: true, berry: "Orange")
+let pineTree = Plant(name: "Pine Tree", habitat: .montain, fruit: true, berry: "Pine")
+let cactus = Plant(name: "Cactus", habitat: .desert, fruit: false)
 
 
-let organisms: [Organism] = [lion, wolf, tree, cactus]
+let organisms: [Organism] = [lion, wolf, tree, cactus, pineTree, squirrel, deer]
 
 for organism in organisms {
     if let animal  = organism as? Animal {
         animal.move()
-        animal.eat()
+        
+        for food in organisms {
+            if food.habitat == animal.habitat {
+                
+                if animal.dietary == .carnivore, food is Animal, food.name != animal.name, animal.commonFood.contains(food.name){
+                    animal.eat(food.name)
+                    
+                } else if animal.dietary == .omnivere, food.name != animal.name, animal.commonFood.contains(food.name){
+                    animal.eat(food.name)
+                    
+                } else if animal.dietary == .herbivore {
+                    if let animalFood = food as? Plant {
+                        if animalFood.fruit, animal.commonFood.contains(animalFood.berry ?? ""){
+                            animal.eat(animalFood.berry ?? "")
+                            
+                        } else if animal.commonFood.contains(animalFood.name){
+                            animal.eat(animalFood.name)
+                        }
+                    }
+                } else {
+                    animal.move()
+                }
+                
+            } else {
+                animal.move()
+            }
+        }
         
     } else if let plant = organism as? Plant {
         plant.performPhotosyntesis()
